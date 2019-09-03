719d342c53489e362c6a0f934125564796cd1840
EasonSun <ysun73@illinois.edu>
2019-09-02 18:04:22 -0700

    Arduino BLE

---
+++ Arduino/BLE_server_StikLight.ino/BLE_server_StikLight.ino.ino
@@ -0,0 +1,73 @@
+/*
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleWrite.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    // TODO send light array on/off matrix as a number; 1 light per byte
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+
+      if (value.length() > 0) {
+        Serial.println("*********");
+        Serial.print("New value: ");
+        for (int i = 0; i < value.length(); i++)
+          Serial.print(value[i]);
+
+        Serial.println();
+        Serial.println("*********");
+      }
+
+
+      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(1000);                       // wait for a second 
+    }
+};
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("1- Download and install an BLE scanner app in your phone");
+  Serial.println("2- Scan for BLE devices in the app");
+  Serial.println("3- Connect to MyESP32");
+  Serial.println("4- Go to CUSTOM CHARACTERISTIC in CUSTOM SERVICE and write something");
+  Serial.println("5- See the magic =)");
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  BLEDevice::init("StikLight");
+  BLEServer *pServer = BLEDevice::createServer();
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  pCharacteristic->setValue("Hello World");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(2000);
+}
--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # A Full Screen Camera App written in Swift
 
+## Camera App
+
 Open the project in XCode, Click on the top level "file" AV Foundation and change the Development Team under Signing. You need to link your developer account first. 
 
 Run: Hook your phone into the Laptop. At the very top bar of XCode, select your phone. Run!
+
+## Arduino 
+Follow this https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/. And yes you need the driver. 
+
+If timeout when uploading, hold the IO0 button and then retry.
+
+## Original text
 
 This is a demo app showing you how to use AVFoundation to build a full-screen camera app in iOS.
 
